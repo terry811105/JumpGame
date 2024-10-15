2b742c97f3cef4ed910c33f586ce368066a7d494
VogtWU <xam93xxxx@gmail.com>
2024-10-16 01:23:01 +0800

    物件跳躍，但無法克服檢查是否在地板上，現在可以連續跳躍不管在地面或空中

--- HebeKissMe/Assets/Scripts/Vogt_CharacterMovement.cs
+++ HebeKissMe/Assets/Scripts/Vogt_CharacterMovement.cs
@@ -6,24 +6,51 @@ using UnityEngine.InputSystem;
 public class Vogt_CharacterMovement : MonoBehaviour
 {
     [SerializeField] private float _speed = 10f;
+    [SerializeField] private float _damping = 0.1f;
+    [SerializeField] private float _jumpForce = 1f;
 
     private Vector2 _moveInput;
     private Vector2 _smoothMovent;
-    private Vector2 _smootthVelocity;
+    private Vector2 _smoothVelocity;
     private Rigidbody2D _rigidbody;
+    private SpriteRenderer _spriteRenderer;
 
     private void Start()
     {
-        _rigidbody=GetComponent<Rigidbody2D>();
-    }
-    private void FixedUpdate()
-    {
-        _smoothMovent = Vector2.SmoothDamp(_smoothMovent, _moveInput, ref _smootthVelocity, 0.5f);
-        _rigidbody.velocity = _moveInput * _speed;
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    public void OnMove(InputValue Value)
+    // 跳躍邏輯
+    private void Jump()
     {
-        _moveInput = Value.Get<Vector2>();
+        Debug.Log("Jumping"); // 輸出跳躍的訊息
+        _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _jumpForce);
+    }
+
+    private void SetMoveInput()
+    {
+        _smoothMovent = Vector2.SmoothDamp(_smoothMovent, _moveInput, ref _smoothVelocity, _damping);
+        _rigidbody.velocity = new Vector2(_smoothMovent.x * _speed, _rigidbody.velocity.y);  // 垂直速度保持不變
+    }
+
+    private void FixedUpdate()
+    {
+        SetMoveInput();
+    }
+
+    public void OnMove(InputValue value)
+    {
+        _moveInput = value.Get<Vector2>();
+    }
+
+    // 跳躍輸入邏輯
+    public void OnJump(InputValue value)
+    {
+        if (value.isPressed)  // 當按下跳躍按鍵時
+        {
+            Debug.Log("Jump Pressed"); // 輸出按下跳躍按鍵的訊息
+            Jump();
+        }
     }
 }
